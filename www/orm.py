c4311074e37e8d65b8224b480f0be36b3b66ccd4
git <18813750773@163.com>
2017-09-12 18:06:49 +0800

    model done

--- www/orm.py
+++ www/orm.py
@@ -14,7 +14,7 @@ def create_pool(loop,**kw):
         user=kw['user'],
         password=kw['password'],
         db=kw['db'],
-        charset=kw.get('charset','utf-8'),
+        charset=kw.get('charset','utf8'),
         autocommit=kw.get('autocommit',True),
         maxsize=kw.get('maxsize',10),
         minsize=kw.get('minsize',1),
@@ -52,17 +52,36 @@ def create_args_string(num):
     for n in range(num):
         L.append('?')
     return ','.join(L)
+
+
 class Field(object):
     def __init__(self,name,column_type,primary_key,default):
         self.name=name
         self.column_type=column_type
         self.primary_key=primary_key
         self.default=default
+
     def __str__(self):
         return '<%s,%s:%s>'%(self.__class__.__name__,self.column_type,self.name)
+
+
 class StringField(Field):
     def __init__(self,name=None,primary_key=False,default=None,ddl='varchar(100)'):
-        super.__init__(name,ddl,primary_key,default)
+        super().__init__(name,ddl,primary_key,default)
+class BooleanField(Field):
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+class IntegerField(Field):
+    def __init__(self,name=None,primary_key=Field,default=0):
+        super().__init__(name,'bigint',primary_key,default)
+class FloatField(Field):
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+class TextField(Field):
+    def __init__(self,name=None,default=None):
+        super().__init__(name,'text',False,default)
+
+
 class ModeMetaclass(type):
     def __new__(cls, name, bases,attrs):
         if name=='Model':
@@ -98,27 +117,28 @@ class ModeMetaclass(type):
         return type.__new__(cls,name,bases,attrs)
 
 
-class Mode(dict,metaclass=ModeMetaclass):
+class Model(dict,metaclass=ModeMetaclass):
     def __init__(self,**kw):
-        super(Mode,self).__init__(**kw)
+        super(Model,self).__init__(**kw)
     def __getattr__(self, key):
         try:
             return self[key]
         except KeyError:
-            raise AttributeError(r"'Model' object has no attribute '$'"%key)
+            raise AttributeError(r"'Model' object has no attribute '%s'"%key)
     def __setattr__(self, key, value):
         self[key]=value
     def getValue(self,key):
         return getattr(self,key,None)
-    def getValueOrDefault(self,key):
-        value=getattr(self,key,None)
+
+    def getValueOrDefault(self, key):
+        value=getattr(self, key,None)
         if value is None:
             field=self.__mappings__[key]
             if field.default is not None:
                 value=field.default() if callable(field.default) else field.default
                 logging.debug('using default value for %s:%s'%(key,str(value)))
                 setattr(self,key,value)
-            return value
+        return value
 
     @classmethod
     @asyncio.coroutine
@@ -133,6 +153,6 @@ class Mode(dict,metaclass=ModeMetaclass):
         args.append(self.getValueOrDefault(self.__primary_key__))
         rows=yield from execute(self.__insert__,args)
         if rows!=1:
-            logging.warn('failed to insert record:affected rows:%s'%rows)
+            logging.warning('failed to insert record:affected rows:%s'%rows)
 
 
