6c99479d04efca09fb912f21ef0dcd1d102c2781
shangxintonghua <18813750773@163.com>
2017-09-12 22:54:32 +0800

    app.py has some code not done

--- www/app.py
+++ www/app.py
@@ -1,13 +1,46 @@
 import logging; logging.basicConfig(level=logging.INFO)
 import asyncio,os,json,time
 from datetime import datetime
+from www.coreweb import add_routes,add_static
+
 from aiohttp import web
 def index(request):
     return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
+
+@asyncio.coroutine
+def logger_factory(app,handler):
+    @asyncio.coroutine
+    def logger(request):
+        logging.info('Request:%s%s'%(request.method,
+                                     request.path))
+        return (yield from handler(request))
+    return logger
+
+@asyncio.coroutine
+def response_factory(app,handler):
+    @asyncio.coroutine
+    def response(request):
+        r=yield from handler(request)
+        if isinstance(r,web.StreamResponse):
+            return  r
+        if isinstance(r,bytes):
+            resp=web.Response(body=r)
+            resp.content_type='application/octet-stream'
+            return resp
+        if isinstance(r,str):
+            resp=web.Response(body=r.encode('utf-8'))
+            resp.content_type='text/html;charset=utf-8'
+            return resp
+
 @asyncio.coroutine
 def init(loop):
-    app=web.Application(loop=loop)
-    app.router.add_route('GET','/',index)
+    app=web.Application(loop=loop,middlewares=[logger_factory,
+                                               response_factory])
+    init_jinja2(app,filter=dict(datetime=datetime_filter))
+    add_routes(app,'handlers')
+    add_static(app)
+
+
     srv=yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
     logging.info('server started at http://127.0.0.1:9000')
     return srv
--- www/coreweb.py
+++ www/coreweb.py
@@ -1,4 +1,8 @@
 import functools
+import asyncio
+import inspect
+import logging
+import os
 
 
 def get(path):
@@ -13,3 +17,65 @@ def get(path):
         wrapper.__route__=path
         return wrapper
     return decorator
+
+
+def post(path):
+    '''
+
+    define decorator @post('/path')
+    '''
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args,**kw):
+            return func(*args,**kw)
+        wrapper.__method__='POST'
+        wrapper.__route__=path
+        return wrapper
+    return decorator
+
+class RequestHandler(object):
+    def __init__(self,app,fn):
+        self._app=app
+        self._func=fn
+    @asyncio.coroutine
+    def __call__(self, request):
+        kw=''
+        r=yield from self._func(**kw)
+        return r
+
+def add_route(app,fn):
+    method=getattr(fn,'__method__',None)
+    path=getattr(fn,'__route__',None)
+    if path is None or method is None:
+        raise ValueError('@get or @ post not defined in %s'%str(fn))
+    if not asyncio.iscoroutine(fn) and not inspect.isgeneratorfunction(fn):
+        fn=asyncio.coroutine(fn)
+    logging.info('add route %s %s =>%s(%s)'%(method,path,fn.__name__,','.join(
+        inspect.signature(fn).parameters.keys()
+    )))
+    app.router.add_route(method,path,RequestHandler(app,fn))
+
+def add_routes(app,module_name):
+    n=module_name.rfind('.')
+    if n==(-1):
+        mod=__import__(module_name,globals(),locals())
+    else:
+        name=module_name[n+1:]
+        mod=getattr(__import__(module_name[:n],globals(),locals(),
+                               [name]),name)
+    for attr in dir(mod):
+        if attr.startswith('_'):
+            continue
+        fn=getattr(mod,attr)
+        if callable(fn):
+            method=getattr(fn,'__method__',None)
+            path=getattr(fn,'__route',None)
+            if method and path:
+                add_route(app,fn)
+def add_static(app):
+    path= os.path.join(os.path.dirname(os.path.abspath(__file__)),'static')
+    app.router.add_static('/static/',path)
+    logging.info('add static %s=>%s'%('/static',path))
+
+
+
