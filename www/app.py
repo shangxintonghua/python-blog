f59f39942140b180c3416c72470a7b679f2f41bc
git <18813750773@163.com>
2017-09-13 16:07:39 +0800

    python mvc can run the trip is done

--- www/app.py
+++ www/app.py
@@ -1,11 +1,10 @@
 import logging; logging.basicConfig(level=logging.INFO)
-import asyncio,os,json,time
+import asyncio,os,json,time,www.orm
 from datetime import datetime
 from www.coreweb import add_routes,add_static
+from jinja2 import Environment,FileSystemLoader
 
 from aiohttp import web
-def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
 
 @asyncio.coroutine
 def logger_factory(app,handler):
@@ -31,16 +30,71 @@ def response_factory(app,handler):
             resp=web.Response(body=r.encode('utf-8'))
             resp.content_type='text/html;charset=utf-8'
             return resp
+        if isinstance(r,dict):
+            template=r.get('__template__')
+            if template is None:
+                resp=web.Response(body=json.dumps(r,ensure_ascii=False,default=lambda o:o.__dict__).encode('utf-8'))
+                resp.content_type='application/json;charset=utf-8'
+                return resp
+            else:
+                resp=web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+                resp.content_type='text/html;charset=utf-8'
+                return resp
+        if isinstance(r,int) and r>=100 and r<600:
+            return web.Response(r)
+        if isinstance(r,tuple) and len(r)==2:
+            t,m=r
+            if isinstance(t,int) and t>=100 and t<600:
+                return web.Response(t,str(m))
+        resp=web.Response(body=str(r).encode('utf-8'))
+        resp.content_type='text/plain;charset=utf-8'
+        return resp
+    return response
+
+
+def init_jinja2(app,**kw):
+    logging.info('init jinja2...')
+    options=dict(
+        autoescape=kw.get('autoescape',True),
+        block_start_string=kw.get('block_start_string','{%'),
+        block_end_string=kw.get('block_end_string','%}'),
+        variable_start_string=kw.get('variable_start_string','{{'),
+        variable_end_string=kw.get('variable_end_string','}}'),
+        auto_reload=kw.get('auto_reload',True)
+    )
+    path=kw.get('path',None)
+    if path is None:
+        path=os.path.join(os.path.dirname(os.path.abspath(__file__)),'templates')
+    logging.info('set jinja2 template path %s'%path)
+    env=Environment(loader=FileSystemLoader(path),**options)
+    filters=kw.get('filters',None)
+    if filters is not None:
+        for name,f in filters.items():
+            env.filters[name]=f
+    app['__templating__']=env
+
+
+def datetime_filter(t):
+    delta=int(time.time()-t)
+    if delta<60:
+        return u'1分钟前'
+    if delta<3600:
+        return u'%s分钟前'%(delta//60)
+    if delta<86400:
+        return u'%s小时前'%(delta//3600)
+    if delta<604800:
+        return u'%s天前'%(delta//86400)
+    dt=datetime.fromtimestamp(t)
+    return u'%s年%月%日'%(dt.year,dt.month,dt.day)
 
 @asyncio.coroutine
 def init(loop):
+    yield from www.orm.create_pool(loop=loop,host='127.0.0.1',port=3306,user='root',password='',db='awesome')
     app=web.Application(loop=loop,middlewares=[logger_factory,
                                                response_factory])
-    init_jinja2(app,filter=dict(datetime=datetime_filter))
+    init_jinja2(app,filters=dict(datetime=datetime_filter))
     add_routes(app,'handlers')
     add_static(app)
-
-
     srv=yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
     logging.info('server started at http://127.0.0.1:9000')
     return srv
---
+++ www/handlers.py
@@ -0,0 +1,18 @@
+from www.coreweb import get,post
+from www.models import User,Comment,Blog,next_id
+import time
+
+
+
+@get('/')
+def index(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200)
+    ]
+    return {
+        '__template__':'blogs.html',
+        'blogs':blogs
+    }
